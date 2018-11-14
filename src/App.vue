9da017d07ea19146ee1da6b3284831ad7714598b
kamyochae <1465678596@qq.com>
2018-11-14 15:03:38 +0800

    add stylus and stylus-loader

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div id="app"> 
+    <Home/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Home from './components/Home'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Home",
+    data:function(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
